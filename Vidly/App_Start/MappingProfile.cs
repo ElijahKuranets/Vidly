5dd057af10f26f0a679d0f73e3cfe80a83fa4771
илья куранец <ilia.kuranets@gmail.com>
2019-11-25 00:36:59 +0300

    Auto Mapper

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        //MapperConfiguration mapFromConfigToDto = new MapperConfiguration(cfg => cfg.CreateMap<Customer, CustomerDto>());
+        //MapperConfiguration mapFromDtoToConfig = new MapperConfiguration(cfg => cfg.CreateMap<CustomerDto, Customer>());
+
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
